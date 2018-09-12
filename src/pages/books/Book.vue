7d8ca867f29ebf57849bd8e2d162533ed4c58d64
范思特 <fansite@fansitedeMacBook-Pro.local>
2018-09-12 15:56:30 +0800

    err

--- src/pages/books/Book.vue
+++ src/pages/books/Book.vue
@@ -1,27 +1,24 @@
 <template>
-  <div id="persons">
-    <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="doLogin">获取用户信息</button>
-  </div>
-  <!-- <div>book</div> -->
+  <div>book</div>
 </template>
 
 <script>
-import config from "../../config"
-import qcloud from "wafer2-client-sdk"
+// import config from "../../config"
+// import qcloud from "wafer2-client-sdk"
 export default {
-  methods: {
-      doLogin: function (e) {
-        qcloud.setLoginUrl(config.loginUrl)
-        qcloud.login({
-          success: function (userInfo) {
-            console.log('登录成功', userInfo)
-          },
-          fail: function (err) {
-            console.log('登录失败1', err)
-          }
-        })
-      }
-    }
+  // methods: {
+  //     doLogin: function (e) {
+  //       qcloud.setLoginUrl(config.loginUrl)
+  //       qcloud.login({
+  //         success: function (userInfo) {
+  //           console.log('登录成功', userInfo)
+  //         },
+  //         fail: function (err) {
+  //           console.log('登录失败1', err)
+  //         }
+  //       })
+  //     }
+  //   }
 }
 </script>
 
--- src/pages/me/Me.vue
+++ src/pages/me/Me.vue
@@ -1,12 +1,96 @@
 <template>
   <div>
-   me
+   <div class="container"> 
+   	<div class="userinfo" > 
+   		<img :src="userinfo.avatarUrl" alt="">
+   		 <p>{{userinfo.nickName}}</p> 
+   		</div>
+	</div>
+	<button v-if='userinfo.openId' class='btn'>添加图书</button>
+   <button v-else open-type="getUserInfo" lang="zh_CN" class='btn' @getuserinfo="login">点击登录</button>
   </div>
 </template>
 
 <script>
+import config from "@/config"
+import qcloud from "wafer2-client-sdk"
 export default {
-  
+  data(){
+	return{
+		userinfo: { 
+			avatarUrl: 'http://image.shengxinjing.cn/rate/unlogin.png', 
+			nickName: '' 
+		}
+	}
+  },
+  methods:{
+  	getWxLogin: function ({encryptedData, iv, userinfo}) {
+		const self = this
+		wx.login({
+			success:function(loginResult){
+				console.log('loginResult',loginResult)
+				var loginParams = { 
+					code: loginResult.code, 
+					encryptedData: encryptedData, 
+					iv: iv 
+				}
+				qcloud.setLoginUrl(config.loginUrl)
+				qcloud.requestLogin({
+					loginParams,
+					success(){
+						qcloud.request({ 
+							url: config.userUrl, 
+							login: true, 
+							success (userRes) { 
+								console.log('登录成功2') 
+								wx.setStorageSync('userinfo', userRes.data.data)
+								self.userinfo = userRes.data.data 
+							} 
+						})
+					},
+					fail(error){
+						console.log('登录失败1', error)
+					}
+				})
+			},
+			fail:function(loginError){
+				console.log('登录失败2',loginError)
+			}
+		})
+  	},
+  	login(e){
+  		const self = this
+  		console.log(0,e)
+  		wx.getSetting({
+  			success:function(res){
+  				console.log(1)
+  				if (res.authSetting['scope.userInfo']) {
+  					wx.checkSession({
+  						success: function (res) {
+							console.log('登录成功1',res)
+  						},
+  						fail:function(){
+  							console.log(2)
+  							qcloud.clearSession()
+  							var options = { 
+  								encryptedData: e.mp.detail.encryptedData, 
+  								iv: e.mp.detail.iv, 
+  								userinfo: e.mp.detail.userInfo 
+  							}
+  							console.log(3) 
+  							self.getWxLogin(options)
+  						}
+  					})
+				}else{
+					console.log('用户未授权', e.mp.detail.errMsg)
+				}				
+  			},
+  			fail:function(){
+  				console.log(4)
+  			}
+  		})
+  	}
+  }
 }
 </script>
 
