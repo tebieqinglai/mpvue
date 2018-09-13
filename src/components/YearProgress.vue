23e912b0c50637beafc3d24152439b32dd6171fe
范思特 <fansite@fansitedeMacBook-Pro.local>
2018-09-13 19:16:13 +0800

    add persent

--- src/components/YearProgress.vue
+++ src/components/YearProgress.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="progressbar">
-		<progress percent='20' activeColor='#ea5a49'></progress>
-		<p>{{year}}已经过去了</p>
+		<progress :percent='persent' activeColor='#ea5a49'></progress>
+		<p>{{year}}已经过去了{{days}}天</p>
 	</div>
 </template>
 
@@ -10,14 +10,23 @@
 		methods:{
 
 		},
-		compunted:{
+		computed:{
 			year(){
 				return new Date().getFullYear()
 			},
 			days(){
-
+				let start = new Date()
+				start.setMonth(0)
+				start.setDate(1)
+				let offset = new Date().getTime() - start.getTime()
+				return parseInt(offset/1000/60/60/24)
+			},
+			persent(){
+				console.log((this.days*100/365).toFixed(1))
+				return (this.days*100/365).toFixed(1)
 			}
 		}
+		
 	}
 </script>
 
